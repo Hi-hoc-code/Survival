45101c58a2f3dc64d73db54d46fcde8bb0f7b990
luanhuupuannn <luanhuupuan@gmail.com>
2025-03-25 19:30:54 +0700

    clock

---
+++ Assets/Scenes/Luan/script/Clock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+   float time = 0f;
+   public TextMeshProUGUI textClock;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+       time += Time.deltaTime;
+        textClock.text = time.ToString("F2"); // Định dạng chuỗi với 2 chữ số thập phân
+    }
+}
