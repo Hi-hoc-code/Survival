25b3b21321d788f979a169fc1a0600dcc4a066be
luanhuupuannn <luanhuupuan@gmail.com>
2025-03-25 12:09:27 +0700

    enemy1

---
+++ Assets/Scenes/Luan/script/moveEnemy1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveEnemy1 : MonoBehaviour
+{
+    public float moveSpeed =1f; // Tốc độ di chuyển của đối tượng
+
+    private Transform player; // Tham chiếu đến đối tượng Player
+
+    void Start()
+    {
+        // Tìm đối tượng Player dựa trên tag
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("Không tìm thấy đối tượng Player với tag 'Player'");
+        }
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            // Tính toán hướng di chuyển
+            Vector2 direction = (player.position - transform.position).normalized;
+            // Di chuyển đối tượng theo hướng đó
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+
+}
